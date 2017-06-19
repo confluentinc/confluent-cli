77b0356a715a4b0e6647bf05227c920323051c50
Konstantine Karantasis <konstantine@confluent.io>
2017-06-19 10:48:09 -0500

    CP-466: Support bash 3.x by no passing array arguments to functions.

--- src/oss/confluent.sh
+++ src/oss/confluent.sh
@@ -575,17 +575,23 @@ stop_service() {
 
 service_exists() {
     local service="${1}"
-    exists "${service}" services
+    exists "${service}" "services"
 }
 
 command_exists() {
     local command="${1}"
-    exists "${command}" commands
+    exists "${command}" "commands"
 }
 
 exists() {
     local arg="${1}"
-    local -n list="${2}"
+
+    case "${2}" in
+        "services")
+        local list=( "${services[@]}" ) ;;
+        "commands")
+        local list=( "${commands[@]}" ) ;;
+    esac
 
     local entry=""
     for entry in "${list[@]}"; do
@@ -607,11 +613,11 @@ list_command() {
 }
 
 start_command() {
-    start_or_stop_service "start" services "${@}"
+    start_or_stop_service "start" "services" "${@}"
 }
 
 stop_command() {
-    start_or_stop_service "stop" rev_services "${@}"
+    start_or_stop_service "stop" "rev_services" "${@}"
     return 0
 }
 
@@ -636,7 +642,12 @@ start_or_stop_service() {
     set_or_get_current
     local command="${1}"
     shift
-    local -n list="${1}"
+    case "${1}" in
+        "services")
+        local list=( "${services[@]}" ) ;;
+        "rev_services")
+        local list=( "${rev_services[@]}" ) ;;
+    esac
     shift
     local service="${1}"
     shift
