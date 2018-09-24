0864e6bf217b9a93a4aab03c4e6ce3f1d326ca0c
GRECO, FRANK <fbgrecojr@me.com>
2018-09-24 09:45:17 -0500

    wrong http method

--- http/api_key.go
+++ http/api_key.go
@@ -44,7 +44,7 @@ func (s *APIKeyService) Create(key *schedv1.ApiKey) (*schedv1.ApiKey, *http.Resp
 func (s *APIKeyService) Delete(key *schedv1.ApiKey) (*http.Response, error) {
 	request := &schedv1.DeleteApiKeyRequest{ApiKey: key}
 	reply := new(schedv1.DeleteApiKeyReply)
-	resp, err := s.sling.New().Post("/api/api_keys").BodyJSON(request).Receive(reply, reply)
+	resp, err := s.sling.New().Delete("/api/api_keys").BodyJSON(request).Receive(reply, reply)
 	if err != nil {
 		return resp, errors.Wrap(err, "unable to delete API key")
 	}
