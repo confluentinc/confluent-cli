10247af6074437174a5cbb75285f5b816332e1bf
Joel Hamill <11722533+joel-hamill@users.noreply.github.com>
2019-05-28 18:44:06 -0600

    DOCS-1808 - Add command examples (#187)

--- internal/cmd/kafka/command_topic.go
+++ internal/cmd/kafka/command_topic.go
@@ -49,8 +49,14 @@ func (c *topicCommand) init() {
 	cmd := &cobra.Command{
 		Use:   "list",
 		Short: "List Kafka topics.",
-		RunE:  c.list,
-		Args:  cobra.NoArgs,
+		Example: `
+List all topics.
+
+::
+
+        ccloud kafka topic list`,
+		RunE: c.list,
+		Args: cobra.NoArgs,
 	}
 	cmd.Flags().String("cluster", "", "Kafka cluster ID.")
 	cmd.Flags().SortFlags = false
@@ -59,8 +65,14 @@ func (c *topicCommand) init() {
 	cmd = &cobra.Command{
 		Use:   "create <topic>",
 		Short: "Create a Kafka topic.",
-		RunE:  c.create,
-		Args:  cobra.ExactArgs(1),
+		Example: `
+Create a topic named 'my_topic' with default options.
+
+::
+
+   ccloud kafka topic create my_topic`,
+		RunE: c.create,
+		Args: cobra.ExactArgs(1),
 	}
 	cmd.Flags().String("cluster", "", "Kafka cluster ID.")
 	cmd.Flags().Uint32("partitions", 6, "Number of topic partitions.")
@@ -73,8 +85,15 @@ func (c *topicCommand) init() {
 	cmd = &cobra.Command{
 		Use:   "describe <topic>",
 		Short: "Describe a Kafka topic.",
-		RunE:  c.describe,
-		Args:  cobra.ExactArgs(1),
+		Example: `
+Describe the 'my_topic' topic.
+
+::
+
+
+       ccloud kafka topic describe my_topic`,
+		RunE: c.describe,
+		Args: cobra.ExactArgs(1),
 	}
 	cmd.Flags().String("cluster", "", "Kafka cluster ID.")
 	cmd.Flags().SortFlags = false
@@ -83,8 +102,14 @@ func (c *topicCommand) init() {
 	cmd = &cobra.Command{
 		Use:   "update <topic>",
 		Short: "Update a Kafka topic.",
-		RunE:  c.update,
-		Args:  cobra.ExactArgs(1),
+		Example: `
+Modify the 'my_topic' topic to have a retention period of days ('259200000' milliseconds).
+
+::
+
+    ccloud kafka topic update my_topic --config="retention.ms=259200000"		`,
+		RunE: c.update,
+		Args: cobra.ExactArgs(1),
 	}
 	cmd.Flags().String("cluster", "", "Kafka cluster ID.")
 	cmd.Flags().StringSlice("config", nil, "A comma-separated list of topic configuration ('key=value') overrides for the topic being created.")
@@ -95,8 +120,15 @@ func (c *topicCommand) init() {
 	cmd = &cobra.Command{
 		Use:   "delete <topic>",
 		Short: "Delete a Kafka topic.",
-		RunE:  c.delete,
-		Args:  cobra.ExactArgs(1),
+		Example: `
+Delete the topics 'my_topic' and 'my_topic_avro'. Use this command carefully as data loss can occur.
+
+::
+
+        ccloud kafka topic delete my_topic
+        ccloud kafka topic delete my_topic_avro		`,
+		RunE: c.delete,
+		Args: cobra.ExactArgs(1),
 	}
 	cmd.Flags().String("cluster", "", "Kafka cluster ID.")
 	cmd.Flags().SortFlags = false
@@ -115,8 +147,14 @@ func (c *topicCommand) init() {
 	c.AddCommand(cmd)
 
 	cmd = &cobra.Command{
-		Use:               "consume <topic>",
-		Short:             "Consume messages from a Kafka topic.",
+		Use:   "consume <topic>",
+		Short: "Consume messages from a Kafka topic.",
+		Example: `
+Consume items from the 'my_topic' topic and press 'Ctrl + C' to exit.
+
+::
+
+	ccloud kafka topic consume -b my_topic`,
 		RunE:              c.consume,
 		Args:              cobra.ExactArgs(1),
 		PersistentPreRunE: c.prerunner.AuthenticatedAPIKey(),
