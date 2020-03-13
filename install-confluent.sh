2c408f094ccf2c08888fd9906dd820963e1a65f5
David Hyde <DABH@users.noreply.github.com>
2020-03-13 09:25:51 -0700

    [CLI-419] Update help messages in install scripts for unsupported platforms (#411)

--- install-confluent.sh
+++ install-confluent.sh
@@ -82,7 +82,7 @@ check_platform() {
     # optional logging goes here
     true
   else
-    log_crit "platform $PLATFORM is not supported.  Make sure this script is up-to-date and file request at https://github.com/${PREFIX}/issues/new"
+    log_crit "platform $PLATFORM is not supported.  Please contact Confluent support if you believe this is a mistake."
     exit 1
   fi
 }
